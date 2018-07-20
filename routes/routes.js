ea06f040339e10ef8c45d2dab836fa41e3f33b64
Kiyong <gy8971@gmail.com>
2018-07-20 18:23:36 +0900

    Add routes/routes.js File

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+/*
+ Default module
+*/
+const express = require('express');
+const router = express.Router();
+
+router.use('/user', require('./user/user_routes'));
+router.use('/order', require('./order/order_routes'));
+router.use('/mypage', require('./mypage/mypage_routes'));
+router.use('/home', require('./home/home_routes'));
+//router.use('/paytest',require('./paytest'))
+
+module.exports = router;
