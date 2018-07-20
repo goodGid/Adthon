7e3141dbb2fff1d5725aa40731f8eb1c323b2532
Kiyong <gy8971@gmail.com>
2018-07-20 18:20:12 +0900

    Init Commit

--- app.js
+++ app.js
@@ -1,49 +1,76 @@
-const winston = require('winston');
-const fs = require('fs');
-const logDir = 'log';
+/*
+ Default module
+*/
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+app.use((req, res, next) => {
+  res.r = (result) => {
+      res.json({
+          status: true,
+          message: "success",
+          result,
+      });
+  };
+  next();
+});
+/*
+ Custom module
+*/
+const routes = require('./routes/routes');
+const jwt = require('./config/secretKey');
+
+/*
+ app.set
+*/
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+app.set('jwt-secret', jwt.secret);
+
+/*
+ app.use
+*/
+app.use(helmet());
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public_data')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/',routes);
 
 
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
-}
-const tsFormat = () => (new Date()).toLocaleTimeString();
-const logger = winston.createLogger({
-  transports: [
-    new (winston.transports.Console)({
-      timestamp: tsFormat,
-      level: 'info'
-    }),
-    new (winston.transports.File)({
-      level: 'info',
-      filename: `${logDir}/logs.log`,
-      timestamp: tsFormat,
-      maxsize:1000000,
-      maxFiles:5
-    })
-  ]
+// error handler
+require('./ErrorHandler')(app);
+
+app.use(function(req, res, next)
+{
+  var err = new Error('Not Found');
+  err.status = 404;
+  err.path = req.path;
+  log.error(err);
+  next(err);
 });
 
+// Error handler
+app.use(function(err, req, res, next)
+{
+  res.locals.message = err.message;
+  console.log("res.locals.message error : " + res.locals.message);
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log("res.locals.error error : " + res.locals.error);
 
-// var logger = winston.createLogger({
-//   transports: [
-//     new (winston.transports.Console)(),
-//     new (winston.transports.File)({ filename: 'somefile.log' })
-//   ]
-// });
+  res.status(err.status || 500);
+  res.render('error',{errLog : res.locals.error});
+});
 
-logger.info('==========');
-logger.log('info', 'Hello distributed log files!');
-logger.info('Hello again distributed logs');
-logger.error('Hello again distributed logs');
+module.exports = app;
 
-// {"level":"info","message":"Hello distributed log files!"}
-// {"message":"Hello again distributed logs","level":"info"}
-
-
-
-
-// winston.log('info', 'Hello distributed log files!');
-// winston.info('Hello again distributed logs');
-
-// winston.level = 'debug';
-// winston.log('debug', 'Now my debug messages are written to console!');
