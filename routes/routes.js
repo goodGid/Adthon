be535c659eaf7627d59109338a83e400afe5dc2c
Kiyong <gy8971@gmail.com>
2018-07-20 18:25:47 +0900

    Init Commit

--- routes/routes.js
+++ routes/routes.js
@@ -4,10 +4,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/user', require('./user/user_routes'));
-router.use('/order', require('./order/order_routes'));
-router.use('/mypage', require('./mypage/mypage_routes'));
-router.use('/home', require('./home/home_routes'));
-//router.use('/paytest',require('./paytest'))
+// router.use('/user', require('./user/user_routes'));
+// router.use('/mypage', require('./mypage/mypage_routes'));
 
 module.exports = router;
